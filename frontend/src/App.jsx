a5f4c146bbe69763c26b8352a0eef6bb8de29b89
Armaan100 <agogoi_be23@thapar.edu>
2025-11-14 08:51:43 +0530

    Added Tailwind css configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="text-lg text-red-500" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
