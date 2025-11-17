f1b7a6d9209d9b3f6ae980d8ffc641c6c7b43a13
Armaan100 <agogoi_be23@thapar.edu>
2025-11-17 16:20:52 +0530

    New modification Plus

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:3000';
-const API_BASE_URL = 'http://13.202.153.166:3000/';
+const API_BASE_URL = 'https://13.202.153.166:3000/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
